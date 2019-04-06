448d5765f4405127f9654e6269da10acf7441e1f
einstruong <46521463+einstruong@users.noreply.github.com>
2019-04-06 21:07:03 +0700

    initial commit
    initial commit

---
+++ precache-manifest.ec1f1e724f7284f73b6c0a0ea91528d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "31bad8d4fc4413d54f8c19930d3739b9",
+    "url": "/nhothspotify/static/media/spotify_logo.31bad8d4.png"
+  },
+  {
+    "revision": "276f273d11f8b9dbc0a9c55bb38ea8c6",
+    "url": "/nhothspotify/static/media/spotify.276f273d.gif"
+  },
+  {
+    "revision": "ce275db806902796bc15cbc05677f268",
+    "url": "/nhothspotify/static/media/loadingNes.ce275db8.gif"
+  },
+  {
+    "revision": "3aaaadcfdaa7c82bcee829ef629658d7",
+    "url": "/nhothspotify/static/media/loading.3aaaadcf.gif"
+  },
+  {
+    "revision": "b981d6c472041398f220",
+    "url": "/nhothspotify/static/js/runtime~main.803b0687.js"
+  },
+  {
+    "revision": "a2979d08af830ed09f80",
+    "url": "/nhothspotify/static/js/main.22c67372.chunk.js"
+  },
+  {
+    "revision": "5f3503b27a49e4f34d0b",
+    "url": "/nhothspotify/static/js/2.1203f525.chunk.js"
+  },
+  {
+    "revision": "a2979d08af830ed09f80",
+    "url": "/nhothspotify/static/css/main.9da32912.chunk.css"
+  },
+  {
+    "revision": "5f3503b27a49e4f34d0b",
+    "url": "/nhothspotify/static/css/2.b127c196.chunk.css"
+  },
+  {
+    "revision": "e484b4ec2a028b4ab99adfcfe9b01489",
+    "url": "/nhothspotify/index.html"
+  }
+];
